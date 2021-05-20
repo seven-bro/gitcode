6991dec078be8e3e21e6fa4c6190334e4a26d176
ckb <1298171361@qq.com>
2021-05-20 17:45:34 +0800

    我的idea提交git

--- maven-IDEA/git_code/src/com/gec/dao/UserDao.java
+++ maven-IDEA/git_code/src/com/gec/dao/UserDao.java
@@ -2,4 +2,12 @@ package com.gec.dao;
 
 public class UserDao {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
